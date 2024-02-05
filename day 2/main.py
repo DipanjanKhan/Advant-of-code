53222e337247c48bf0e7683f059e870a53db031d
DipanjanKhan <125537480+DipanjanKhan@users.noreply.github.com>
2024-02-05 22:41:41 +0530

    Update main.py

--- day 2/main.py
+++ day 2/main.py
@@ -1,27 +1,31 @@
 import re
 
-with open("D:\Dipu\Practice\Python\Advant of code\day 2\input.txt",'r') as f:
+with open("input.txt",'r') as f:
     str = f.readlines()
 
 no = 0
+lists = []
 count = 0
 for i in str:
     no += 1
-    blue = 0
-    green = 0
-    red = 0
-    for j in range(len(i)):
-        if i[j] == "b":
-            st = i[j-4: j]
-            blue = int(re.findall(r"\d",st)[0]) + blue
-        if i[j] == "r":
-            st = i[j-4: j]
-            red = int(re.findall(r"\d",st)[0]) + red
-        if i[j] == "g":
-            st = i[j-4: j]
-            green = int(re.findall(r"\d",st)[0]) + green
-    if red <= 12 and green <= 13 and blue <= 14:
-        count += no
-        print(no)
-    
+    st = re.split(r'[:;,]', i)
+    flag = True
+    for j in st:
+        l = [j.replace('\n', '').strip().split(' ')]
+        
+        if l[0][1]=='blue' and int(l[0][0]) > 14:
+            flag = False
+            break
+
+        if l[0][1]=='red' and int(l[0][0]) > 12:
+            flag = False
+            break
+
+        if l[0][1]=='green' and int(l[0][0]) > 13:
+            flag = False
+            break
+
+    if flag:
+        count = count + no
+
 print(count)
